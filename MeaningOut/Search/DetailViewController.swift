0a8613fe553e976423697b1ef1420bdba0f4db00
jeongyun-kim <kkanyo04@naver.com>
2024-06-15 00:13:07 +0900

    상세화면 내 웹뷰 구현 / ATS YES로 변경 / 선택한 아이템 제목으로 네비게이션 타이틀 변경

--- MeaningOut/Search/DetailViewController.swift
+++ MeaningOut/Search/DetailViewController.swift
@@ -7,29 +7,43 @@
 
 import UIKit
 import SnapKit
+import WebKit
 
 class DetailViewController: UIViewController, SetupView {
    
     lazy var selectedItem: SearchItem = SearchItem(title: "", link: "", image: "", lprice: "", mallName: "", productId: "")
    
+    lazy var webView = WKWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupHierarchy()
         setupConstraints()
         setupUI()
+        configureWebView()
     }
 
     func setupHierarchy() {
-        
+        view.addSubview(webView)
     }
     
     func setupConstraints() {
-        
+        webView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func setupUI() {
         view.backgroundColor = .systemBackground
         navigationItem.title = selectedItem.replacedTitle
+    }
+    
+    private func configureWebView() {
+        // url로 전환 -> request 생성 -> request로 load
+        // http 링크위해 ATS = YES
+        let url = URL(string: selectedItem.link)
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
     
 }
