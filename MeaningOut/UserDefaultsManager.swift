cab62b8ba4b282a25eaa1833e4938721a60ca352
jeongyun-kim <kkanyo04@naver.com>
2024-06-30 16:13:11 +0900

    Refactor: UserDefaulsManager에 propertyWrapper 활용해 코드 간소화

--- MeaningOut/UserDefaultsManager.swift
+++ MeaningOut/UserDefaultsManager.swift
@@ -7,75 +7,41 @@
 
 import Foundation
 
+@propertyWrapper
+struct UserDefaultsWrapper<T> {
+    let key: String
+    let defaultValue: T
+    
+    var wrappedValue: T {
+        get { return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+    
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+}
+
 class UserDefaultsManager {
     private init () { }
     static let shared = UserDefaultsManager()
-    let standard = UserDefaults.standard
     
     // 유저인지 확인하기 (default = false)
-    var isUser: Bool {
-        get {
-            standard.bool(forKey: "isUser")
-        }
-        set {
-            standard.setValue(newValue, forKey: "isUser")
-        }
-    }
+    @UserDefaultsWrapper(key: "isUser", defaultValue: false) var isUser: Bool
     
-    var userProfileImage: String {
-        get {
-            standard.string(forKey: "profileImage") ?? ProfileImage.randomImage.imageName
-        }
-        set {
-            standard.setValue(newValue, forKey: "profileImage")
-        }
-    }
+    @UserDefaultsWrapper(key: "profileImage", defaultValue: ProfileImage.randomImage.imageName) var userProfileImage: String
+   
+    @UserDefaultsWrapper(key: "userName", defaultValue: "") var userName: String
     
-   var userName: String {
-        get {
-            standard.string(forKey: "userName") ?? ""
-        }
-        set {
-            standard.setValue(newValue, forKey: "userName")
-        }
-    }
+    @UserDefaultsWrapper(key: "joinDate", defaultValue: "") var joinDate: String
     
-    var joinDate: String {
-        get {
-            standard.string(forKey: "joinDate") ?? ""
-        }
-        set {
-            standard.setValue(newValue, forKey: "joinDate")
-        }
-    }
+    @UserDefaultsWrapper(key: "searchKeywords", defaultValue: []) var searchKeywords: [String]
     
-    var searchKeywords: [String] {
-        get {
-            standard.stringArray(forKey: "searchKeywords") ?? []
-        }
-        set {
-            standard.setValue(newValue, forKey: "searchKeywords")
-        }
-    }
+    @UserDefaultsWrapper(key: "likedItemId", defaultValue: []) var likedItemId: [String]
     
-    var likedItemId: [String] {
-        get {
-            standard.stringArray(forKey: "likedItemId") ?? []
-        }
-        set {
-            standard.setValue(newValue, forKey: "likedItemId")
-        }
-    }
-    
-    var likeCnt: Int {
-        get {
-            standard.integer(forKey: "likeCnt")
-        }
-        set {
-            standard.setValue(newValue, forKey: "likeCnt")
-        }
-    }
-    
+    @UserDefaultsWrapper(key: "likeCnt", defaultValue: 0) var likeCnt: Int
+
     func deleteAllDatas() {
         for key in UserDefaults.standard.dictionaryRepresentation().keys {
             UserDefaults.standard.removeObject(forKey: key.description)
